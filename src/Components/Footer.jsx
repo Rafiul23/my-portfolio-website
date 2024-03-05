efc04e063e995e5a0b4098ba807a17c33ec81c09
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-05 16:05:43 +0600

    added new footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-4 py-4 bg-black text-base-content">
+        <footer className="footer footer-center p-10 bg-black text-primary-content">
             <aside>
-                <p className='text-alpha font-bold'>Copyright © 2023 - All right reserved by Md. Rafiul Islam</p>
+
+                <p className="font-bold">
+                    Md. Rafiul Islam
+                </p>
+                <p>Copyright © 2024 - All right reserved</p>
             </aside>
+            <nav>
+                <div className="grid grid-flow-col gap-4">
+                    <a className="text-2xl" href="https://www.facebook.com/rafiulislam.shaon.5"><FaFacebookF /></a>
+                    <a className="text-2xl" href="https://www.linkedin.com/in/md-rafiul-islam-645681112/"><FaLinkedin /></a>
+                    <a className="text-2xl" href="https://github.com/rafiul23/"><FaGithub /></a>
+                </div>
+            </nav>
         </footer>
     );
 };
