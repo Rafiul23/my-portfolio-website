6d45f401b84511e5d6884389cad5f292bfd1579a
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-05 19:19:47 +0600

    footer design is slightly changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,14 @@ const Footer = () => {
         <footer className="footer footer-center p-10 bg-black text-primary-content">
             <aside>
 
-                <p className="font-bold">
+                <p className="font-bold text-3xl text-alpha">
                     Md. Rafiul Islam
                 </p>
                 <p>Copyright © 2024 - All right reserved</p>
+                <p>I am available on</p>
             </aside>
             <nav>
-                <div className="grid grid-flow-col gap-4">
+                <div className="grid grid-cols-3 gap-3">
                     <a target="_blank" className="text-2xl" href="https://www.facebook.com/rafiulislam.shaon.5"><FaFacebookF /></a>
                     <a target="_blank" className="text-2xl" href="https://www.linkedin.com/in/md-rafiul-islam-645681112/"><FaLinkedin /></a>
                     <a target="_blank" className="text-2xl" href="https://github.com/rafiul23/"><FaGithub /></a>
