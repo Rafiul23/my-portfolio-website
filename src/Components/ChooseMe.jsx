495cb3766726ccc5f257dda4c497bfb11297c1f9
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-21 15:04:34 +0600

    added responsiveness at why choose me

--- src/Components/ChooseMe.jsx
+++ src/Components/ChooseMe.jsx
@@ -11,8 +11,8 @@ const ChooseMe = () => {
 
             <div className="hero min-h-screen bg-gradient-to-r from-slate-700 to-slate-800 border border-alpha rounded-lg">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src={chooseme} className="w-1/2 rounded-full " />
-                    <div className="p-4 w-1/2">
+                    <img src={chooseme} className="md:w-1/2 w-full rounded-full " />
+                    <div className="p-4 md:w-1/2 w-full">
                         
                         <ul className="text-alpha font-semibold space-y-3 text-lg py-4 list-disc">
                             <li className="">Proficient in HTML5, CSS3, JavaScript (ES6+)</li>
