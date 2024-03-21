b059bfe59992655fe74b3c255fe184f7e3e2a3c4
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-03-21 15:54:42 +0600

    contact page is eddited

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
                 <div className="hero-content flex-col md:flex-row-reverse">
                     <img src={contact} className="md:w-1/2 w-full rounded-lg shadow-xl" />
                     <div className='md:w-1/2 w-full'>
-                        <h1 className="lg:text-5xl md:text-3xl text-xl pl-4 text-alpha font-bold">Contact with me by:</h1>
+                        <h1 className="lg:text-5xl md:text-3xl text-xl pl-4 text-alpha font-bold">Contact with me via:</h1>
                         <div className='space-y-2 p-4 mt-2 font-semibold'>
                         <p>Email: <span className='text-blue-600'>rislamshaon95@gmail.com</span></p>
                         <p>Phone: <span className='text-blue-600'>01980114434</span></p>
